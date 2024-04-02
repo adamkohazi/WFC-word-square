0e2a87fea7d127ace5fc961f3fa823924ad177e2
adamkohazi <adam.kohazi@gmail.com>
2024-04-02 08:02:49 +0200

    general refractoring

--- word_square_generator.py
+++ word_square_generator.py
@@ -1,7 +1,15 @@
 import wordmatrix
 import wavefunction
 
-size = [4,5]
+size = [5,4]
+
+def optimize_dictionary (lengths, dict):
+    opt_dict = []
+    for word in dict:
+        if len(word) in lengths:
+            if word.isalpha():
+                opt_dict.append(word.lower())
+    return opt_dict
 
 def get_dictionary_word_list(filename):
     with open(filename, encoding="utf_8") as f:
@@ -11,6 +19,7 @@ def get_dictionary_word_list(filename):
 # Loading words to a dictionary for generation
 #dict = get_dictionary_word_list("dictionary_EN.txt")
 dict = get_dictionary_word_list("google-10000-english.txt")
+dict = optimize_dictionary(size, dict)
 
 table = wordmatrix.Wordmatrix(size, dict)
 wfc = wavefunction.Wavefunction(table)
