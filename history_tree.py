2daf4450dc7953768618681a0353c98369da8509
adamkohazi <109677549+adamkohazi@users.noreply.github.com>
2024-04-12 14:02:28 +0200

    Made iteration more efficient by move tree and DFS.

--- history_tree.py
+++ history_tree.py
@@ -1,16 +1,15 @@
 from anytree import NodeMixin, RenderTree
 
 class Move(object):  # Represents a single move
-    deadEnd = False
-    def __init__(self, x, y, letter):
+    def __init__(self, x, y, letter, matrix):
         self.x = x
         self.y = y
         self.letter = letter
+        self.wordmatrix = matrix
 
 class MoveNode(Move, NodeMixin):  # Add Node feature
-    def __init__(self, x, y, letter, parent=None, children=None):
-        super(MoveNode, self).__init__(x, y, letter)
-        #super(Move, self).__init__(x, y, letter)
+    def __init__(self, x, y, letter, matrix, parent=None, children=None):
+        super(MoveNode, self).__init__(x, y, letter, matrix)
         self.parent = parent
         if children:  # set children only if given
             self.children = children
--- wavefunction.py
+++ wavefunction.py
@@ -1,91 +1,65 @@
-import random
 import history_tree
 from anytree import RenderTree
+from copy import deepcopy
 
 class Wavefunction(object):
 
     def __init__(self, matrix):
-        self.wordmatrix = matrix
-        self.history = []
-        self.wordmatrix.print_options()
-        self.root = history_tree.MoveNode(0, 0, '-', parent=None, children=None)
+        self.root = history_tree.MoveNode(0, 0, '-',  matrix, parent=None, children=None)
         self.currentNode = self.root
 
     def run(self):
-        while not self.wordmatrix.is_fully_defined():
-            self.iterate()
-        
-        self.wordmatrix.print_defined()
+        """Runs iterations until the matrix is fully solved, or out of options.
+        """
+        while not self.currentNode.wordmatrix.is_fully_defined():
+            if self.currentNode == self.root and self.currentNode.wordmatrix.is_deadend():
+                print("No more options")
+                print(self.currentNode.wordmatrix.blacklist)
+                break
+            else:
+                self.iterate()
+
+        self.print_tree()
+        self.currentNode.wordmatrix.print_options()
+        self.currentNode.wordmatrix.print_defined()
     
     def iterate(self):
         """Performs a single iteration of the Wavefunction Collapse
         Algorithm.
         """
-        #Check if wordmatrix is possible
-        if not self.wordmatrix.is_deadend():
-            self.wordmatrix.backup()
-
-            #Find the co-ordinates of minimum entropy
-            x, y = self.find_min_entropy()
-
-            #Collapse the wavefunction at these co-ordinates
-            letter = self.wordmatrix.define(x, y)
-
-            #Take a note of the move
-            self.history.append([x,y,letter])
-            self.currentNode = history_tree.MoveNode(x, y, letter, parent=self.currentNode)
-            print("letter added: x=", x, ", y=", y, ": ", letter)
-            print("history length: ", len(self.wordmatrix.history))
-
-            #Propagate the consequences of this collapse
-            self.wordmatrix.update_possibilities()
-        
-        #If it is a deadend, backtrack
-        else:
-            self.currentNode.deadEnd = True
-            if len(self.history) == 0:
-                print("No more options")
-                print(self.wordmatrix.blacklist)
-                quit()
-            self.wordmatrix.restore()
+        # Figure if we should move up or down the tree (new move or backtrack)
+        if self.currentNode.wordmatrix.is_deadend():
+            # Backtrack
+            # Note the previous move
+            x = self.currentNode.x
+            y = self.currentNode.y
+            letter = self.currentNode.letter
+            print("letter removed: (", x, ",", y, "): ", letter)
+            # Revert wrong move
             self.currentNode = self.currentNode.parent
-            
-            #Learn from the mistake
-            x, y, letter = self.history.pop()
-            self.wordmatrix.add_blacklist(x, y, letter)
-            print("letter removed: ", x, ", ", y, ": ", letter)
-            print("history length: ", len(self.wordmatrix.history))
+            # Learn from the mistake
+            self.currentNode.wordmatrix.add_blacklist(x, y, letter)
 
-            #Update matrix based on new blacklist
-            self.wordmatrix.update_possibilities()
+        else:
+            # New move
+            # Find the coordinates of minimum entropy
+            x, y = self.currentNode.wordmatrix.find_min_entropy()
+            # Collapse the wavefunction at these coordinates
+            new_matrix = deepcopy(self.currentNode.wordmatrix)
+            letter = new_matrix.define(x, y)
+            # Make a note of move
+            self.currentNode = history_tree.MoveNode(x, y, letter, new_matrix, parent=self.currentNode)
+            print("letter added:   (", x, ",", y, "): ", letter)
         
-        #Print results
+        # Propagate changes, finish on a clean state
+        self.currentNode.wordmatrix.update_possibilities()
+        
+        # Debug printing
         #self.wordmatrix.print_defined()
+        #self.print_tree()
+        #self.currentNode.wordmatrix.print_options()
+
+    def print_tree(self):
         for pre, _, node in RenderTree(self.root):
             treestr = u"%s%s%s%s" % (pre, node.x, node.y, node.letter)
             print(treestr.ljust(8))
-        self.wordmatrix.print_options()
-
-
-    def find_min_entropy(self):
-        """Returns the co-ords of the location whose wavefunction has
-        the lowest entropy.
-        """
-        
-        min_entropy_coords = (0, 0)
-        min_entropy = 1000
-
-        for y in range(self.wordmatrix.height):
-            for x in range(self.wordmatrix.width):
-                if self.wordmatrix.is_defined(x,y):
-                    continue
-
-                entropy = self.wordmatrix.shannon_entropy(x,y)
-                # Add some noise to mix things up a little
-                entropy = entropy - (random.random() / 1000)
-
-                if entropy < min_entropy:
-                    min_entropy = entropy
-                    min_entropy_coords = (x, y)
-
-        return min_entropy_coords
