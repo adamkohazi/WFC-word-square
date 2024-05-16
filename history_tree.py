a9e06827965368e8980370725c6262a31e46c5b8
adamkohazi <109677549+adamkohazi@users.noreply.github.com>
2024-05-16 16:34:31 +0200

    Started work on GUI

--- history_tree.py
+++ history_tree.py
@@ -1,15 +1,15 @@
 from anytree import NodeMixin
 
 class Move(object):  # Represents a single move
-    def __init__(self, x, y, letter, matrix):
+    def __init__(self, x, y, letter, crossword):
         self.x = x
         self.y = y
         self.letter = letter
-        self.wordmatrix = matrix
+        self.crossword = crossword
 
 class MoveNode(Move, NodeMixin):  # Add Node feature
-    def __init__(self, x, y, letter, matrix, parent=None, children=None):
-        super(MoveNode, self).__init__(x, y, letter, matrix)
+    def __init__(self, x, y, letter, crossword, parent=None, children=None):
+        super(MoveNode, self).__init__(x, y, letter, crossword)
         self.parent = parent
         if children:  # set children only if given
             self.children = children
--- wavefunction.py
+++ wavefunction.py
@@ -5,8 +5,8 @@ import time
 
 class Wavefunction(object):
 
-    def __init__(self, matrix):
-        self.root = history_tree.MoveNode(0, 0, '-',  matrix, parent=None, children=None)
+    def __init__(self, crossword):
+        self.root = history_tree.MoveNode(0, 0, '-',  crossword, parent=None, children=None)
         self.currentNode = self.root
         self.treelevel = 0
         self.i = 0
@@ -14,22 +14,22 @@ class Wavefunction(object):
 
     #@profile
     def run(self):
-        """Runs iterations until the matrix is fully solved, or out of options.
+        """Runs iterations until the crossword is fully solved, or out of options.
         """
 
         startTime = time.perf_counter()
-        while not self.currentNode.wordmatrix.isFullyDefined():
-            if self.currentNode == self.root and self.currentNode.wordmatrix.isDeadend():
+        while not self.currentNode.crossword.isFullyDefined():
+            if self.currentNode == self.root and self.currentNode.crossword.isDeadend():
                 print("No more options")
-                #print(self.currentNode.wordmatrix.blacklist)
+                #print(self.currentNode.crossword.blacklist)
                 break
             else:
                 self.iterate()
         
         endTime = time.perf_counter()
         #self.print_tree()
-        #self.currentNode.wordmatrix.printOptions()
-        #self.currentNode.wordmatrix.printDefined()
+        #self.currentNode.crossword.printOptions()
+        #self.currentNode.crossword.printDefined()
         print("%d updates in total." % self.totalUpdates)
         print("Total time: %.2gs" % (endTime-startTime))
     
@@ -38,7 +38,7 @@ class Wavefunction(object):
         Algorithm.
         """
         # Figure if we should move up or down the tree (new move or backtrack)
-        if self.currentNode.wordmatrix.isDeadend() or not self.currentNode.wordmatrix.isFullyValid():
+        if self.currentNode.crossword.isDeadend() or not self.currentNode.crossword.isFullyValid():
             # Backtrack
             self.treelevel -= 1
             # Note the previous move
@@ -49,33 +49,33 @@ class Wavefunction(object):
             # Revert wrong move
             self.currentNode = self.currentNode.parent
             # Learn from the mistake
-            self.currentNode.wordmatrix.addToBlacklist((x, y), letter)
+            self.currentNode.crossword.addToBlacklist((x, y), letter)
 
         else:
             # New move
             self.treelevel += 1
             # Find the coordinates of minimum entropy
-            x, y = self.currentNode.wordmatrix.findMinEntropy()
+            x, y = self.currentNode.crossword.findMinEntropy()
             # Collapse the wavefunction at these coordinates
-            new_matrix = deepcopy(self.currentNode.wordmatrix) #TODO: optimize
+            new_matrix = deepcopy(self.currentNode.crossword) #TODO: optimize
             letter = new_matrix.define((x, y))
             # Make a note of move
             self.currentNode = history_tree.MoveNode(x, y, letter, new_matrix, parent=self.currentNode)
             print("letter added:   (", x, ",", y, "): ", letter," - ",self.treelevel)
         
         # Propagate changes, finish on a clean state
-        self.totalUpdates += self.currentNode.wordmatrix.updateOptions()
+        self.totalUpdates += self.currentNode.crossword.updateOptions()
 
         self.i += 1
         #if self.i % 100 == 0:
             #self.print_tree()
-            #self.currentNode.wordmatrix.printOptions()
-            #print(self.currentNode.wordmatrix.blacklist)
+            #self.currentNode.crossword.printOptions()
+            #print(self.currentNode.crossword.blacklist)
         
         # Debug printing
-        self.currentNode.wordmatrix.printDefined()
+        self.currentNode.crossword.printDefined()
         #self.print_tree()
-        #self.currentNode.wordmatrix.print_options()
+        #self.currentNode.crossword.print_options()
 
     def print_tree(self):
         for pre, _, node in RenderTree(self.root):
